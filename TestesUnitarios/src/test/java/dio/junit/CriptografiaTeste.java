0dfe38e85994a5619ad457d1c9d092c20d6bb5f8
aBrunaMiranda <122480583+aBrunaMiranda@users.noreply.github.com>
2024-02-02 11:31:21 -0300

    Update CriptografiaTeste.java
    Adição de novos testes para contemplar por completo o projeto da Criptografia

--- TestesUnitarios/src/test/java/dio/junit/CriptografiaTeste.java
+++ TestesUnitarios/src/test/java/dio/junit/CriptografiaTeste.java
@@ -6,8 +6,28 @@ import org.junit.jupiter.api.Test;
 public class CriptografiaTeste {
 
     @Test
-    public void validarCriptografia(){
+    public void validarCriptografia() {
         CifraDeCesar palavra = new CifraDeCesar();
         Assertions.assertEquals("rod, pxqgr", palavra.cifraDeCesarTradicional("Ola, mundo"));
     }
+
+    @Test
+    public void validarCriptoLivre(){
+        CifraDeCesar livre = new CifraDeCesar();
+        Assertions.assertEquals("udidho jxlpdudhv vdnxudl", livre.cifraDeCesarLivre("Rafael Guimaraes Sakurai", 3));
+        }
+
+    @Test
+    public void desencriptarTradicional(){
+        CifraDeCesar desencripitarTradicional = new CifraDeCesar();
+        Assertions.assertEquals("ola, mundo", desencripitarTradicional.decifraCesarTradicional("rod, pxqgr"));
+    }
+
+    @Test
+    public void desencriptarLivre(){
+        CifraDeCesar desencripitarLivre = new CifraDeCesar();
+        Assertions.assertEquals("rafael guimaraes sakurai", desencripitarLivre.decifraCesarLivre("udidho jxlpdudhv vdnxudl", 3));
+    }
+
+
 }
